7f826e9e00826e0d5aee2a865bce422744d80b17
BackPick <ryul-@naver.com>
2025-05-14 18:37:33 +0900

    [Silver II] Title: 알파벳 블록, Time: 412 ms, Memory: 93668 KB -BaekjoonHub

---
+++ 백준/Silver/27497. 알파벳 블록/알파벳 블록.java
@@ -0,0 +1,42 @@
+
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int              n   = Integer.parseInt(br.readLine());
+        Deque<Character> dq  = new ArrayDeque<>();
+        Stack<Boolean>   stk = new Stack<>();
+
+        while (n-- > 0) {
+            String input = br.readLine();
+            char   op    = input.charAt(0);
+
+            if (op == '1') {
+                dq.offerLast(input.charAt(2));
+                stk.push(true);
+            } else if (op == '2') {
+                dq.offerFirst(input.charAt(2));
+                stk.push(false);
+            } else if (!dq.isEmpty()) {
+                if (stk.pop()) {
+                    dq.removeLast();
+                } else dq.removeFirst();
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (char c : dq)
+            sb.append(c);
+
+        bw.write(sb.length() > 0 ? sb.toString() : "0");
+        bw.flush();
+    }
+
+}
