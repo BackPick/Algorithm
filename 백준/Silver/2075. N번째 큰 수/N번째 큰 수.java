826b565e9cb417bfb9d0769b35c442d2cb53dfb8
BackPick <ryul-@naver.com>
2025-05-27 01:08:14 +0900

    [Silver III] Title: N번째 큰 수, Time: 880 ms, Memory: 220692 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -1,22 +1,37 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner                sc      = new Scanner(System.in);
-        int                    n       = sc.nextInt();
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        int                    cnt     = n * n;
-        for (int i = 0; i < cnt; i++) {
-            int num = sc.nextInt();
-            if (minHeap.size() < n) {
-                minHeap.offer(num);
-            } else if (minHeap.peek() < num) {
-                minHeap.poll();
-                minHeap.offer(num);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new java.io.OutputStreamWriter(System.out));
+
+        int testCase = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < testCase; i++) {
+            StringTokenizer st     = new StringTokenizer(br.readLine());
+            int             tokens = st.countTokens();
+            for (int j = 0; j < tokens; j++) {
+                int number = Integer.parseInt(st.nextToken());
+                if (pq.size() < testCase) {
+                    pq.offer(number);
+                } else {
+                    if (pq.peek() < number) {
+                        pq.poll();
+                        pq.offer(number);
+                    }
+                }
             }
+
         }
-        sc.close();
-        System.out.println(minHeap.peek());
+        bw.write(pq.peek() + "");
+        bw.flush();
+
     }
 }
