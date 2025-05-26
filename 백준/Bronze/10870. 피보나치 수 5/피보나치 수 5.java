766fa3615bdf0541ab14ff75345af20fb938a195
BackPick <ryul-@naver.com>
2025-05-26 19:47:57 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 100 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -1,13 +1,16 @@
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new java.io.OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
-
-        bw.write(String.valueOf(fibonachi(n)));
+        int n      = Integer.parseInt(br.readLine());
+        int result = fibonachi(n);
+        bw.write(result + "");
         bw.flush();
 
     }
