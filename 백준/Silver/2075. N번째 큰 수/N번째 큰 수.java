541b9c7980bb837fd0a7aebec03fdaf327388fc9
BackPick <ryul-@naver.com>
2025-05-27 01:16:55 +0900

    [Silver III] Title: N번째 큰 수, Time: 756 ms, Memory: 215968 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -1,37 +1,34 @@
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new java.io.OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int testCase = Integer.parseInt(br.readLine());
+        int                    N  = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); // 최소 힙
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for (int i = 0; i < testCase; i++) {
-            StringTokenizer st     = new StringTokenizer(br.readLine());
-            int             tokens = st.countTokens();
-            for (int j = 0; j < tokens; j++) {
-                int number = Integer.parseInt(st.nextToken());
-                if (pq.size() < testCase) {
-                    pq.offer(number);
-                } else {
-                    if (pq.peek() < number) {
-                        pq.poll();
-                        pq.offer(number);
-                    }
+            while (st.hasMoreTokens()) {
+                int num = Integer.parseInt(st.nextToken());
+
+                if (pq.size() < N) {
+                    pq.offer(num);
+                } else if (pq.peek() < num) {
+                    pq.poll();
+                    pq.offer(num);
                 }
             }
-
         }
-        bw.write(pq.peek() + "");
-        bw.flush();
 
+        bw.write(pq.peek() + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
