47493b6c34e954e4fbd1fd0aab5d0094fa8587ec
BackPick <ryul-@naver.com>
2025-05-27 01:31:29 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1004 ms, Memory: 174396 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,32 +1,39 @@
+
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int             n1 = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
+        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new java.io.OutputStreamWriter(System.out));
 
         Map<Integer, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < n1; i++) {
-            int n2 = Integer.parseInt(st.nextToken());
-            map.put(n2, map.getOrDefault(n2, 0) + 1);
+        int             n  = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int inputCard = Integer.parseInt(st.nextToken());
+            map.put(inputCard, map.getOrDefault(inputCard, 0) + 1);
         }
-        int           n3 = Integer.parseInt(br.readLine());
+
         StringBuilder sb = new StringBuilder();
+
+        int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n3; i++) {
-            int     n4     = Integer.parseInt(st.nextToken());
-            Integer result = map.getOrDefault(n4, 0);
-            sb.append(result).append(" ");
+        for (int i = 0; i < m; i++) {
+            int outputCard = Integer.parseInt(st.nextToken());
+            sb.append(map.getOrDefault(outputCard, 0))
+                    .append(" ");
         }
-        br.close();
-        System.out.println(sb.toString().trim());
+
+        bw.write(sb.toString()
+                         .trim());
+        bw.flush();
+
     }
 }
