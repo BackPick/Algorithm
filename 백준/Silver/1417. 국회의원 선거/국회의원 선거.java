5c3b6aad35c54071fd44c153bc46daabb8f920b5
BackPick <ryul-@naver.com>
2025-05-30 17:18:23 +0900

    [Silver V] Title: 국회의원 선거, Time: 112 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
@@ -1,36 +1,40 @@
 
-import java.io.*;
-import java.util.Collections;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new java.io.OutputStreamWriter(System.out));
 
-        PriorityQueue<Integer> pq    = new PriorityQueue<>(Collections.reverseOrder());
-        int                    n     = Integer.parseInt(br.readLine());
-        int                    DASOM = Integer.parseInt(br.readLine());
-        int                    count = 0;
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>((o1, o2) -> o2 - o1);
+
+        int n     = Integer.parseInt(br.readLine());
+        int dasom = Integer.parseInt(br.readLine());
 
         if (n == 1) {
-            System.out.println(0);
-            System.exit(0);
+            bw.write("0");
+            bw.flush();
+            return;
         }
 
         for (int i = 0; i < n - 1; i++) {
-            pq.offer(Integer.parseInt(br.readLine()));
+            priorityQueue.offer(Integer.parseInt(br.readLine()));
         }
-        while (DASOM <= pq.peek()) {
-            int poll = pq.poll();
+        int count = 0;
+        while (priorityQueue.peek() >= dasom) {
+            Integer voter = priorityQueue.poll();
             count++;
-            DASOM++;
-            poll--;
-            if (poll > 0) {
-                pq.offer(poll);
+            voter--;
+            dasom++;
+            if (voter > 0) {
+                priorityQueue.offer(voter);
             }
-
         }
-        System.out.println(count);
+        bw.write(String.valueOf(count));
+        bw.flush();
     }
 }
