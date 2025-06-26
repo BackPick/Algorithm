a45814c6673c4799389684b11d9dab1b8d05f7bd
BackPick <ryul-@naver.com>
2025-06-26 21:58:19 +0900

    [Silver I] Title: 행렬, Time: 200 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Silver/1080. 행렬/행렬.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Main {
+    static int N, M;
+    static int[][] A, B;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        A = new int[N][M];
+        B = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String line = sc.next();
+            for (int j = 0; j < M; j++) {
+                A[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            String line = sc.next();
+            for (int j = 0; j < M; j++) {
+                B[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        int count = 0;
+
+        for (int i = 0; i <= N - 3; i++) {
+            for (int j = 0; j <= M - 3; j++) {
+                if (A[i][j] != B[i][j]) {
+                    flip(i, j);
+                    count++;
+                }
+            }
+        }
+
+        if (isSame()) {
+            System.out.println(count);
+        } else {
+            System.out.println(-1);
+        }
+    }
+
+    static void flip(int x, int y) {
+        for (int i = x; i < x + 3; i++) {
+            for (int j = y; j < y + 3; j++) {
+                A[i][j] ^= 1;
+            }
+        }
+    }
+
+    static boolean isSame() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (A[i][j] != B[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
